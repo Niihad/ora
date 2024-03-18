7a2110198b6a9de80c80e614b9a0a5917113f71b
Niihad <achyle03@gmail.com>
2024-03-18 13:54:44 +0100

    add multilanguage

--- i18n/dictionary.ts
+++ i18n/dictionary.ts
@@ -5,8 +5,8 @@ import { Locale } from "./i18n-config";
 const dictionaries = {
   en: () => import("./dictionaries/en.json").then((module) => module.default),
   fr: () => import("./dictionaries/fr.json").then((module) => module.default),
-  lu: () => import("./dictionaries/fr.json").then((module) => module.default),
-  po: () => import("./dictionaries/fr.json").then((module) => module.default),
+  lu: () => import("./dictionaries/lu.json").then((module) => module.default),
+  po: () => import("./dictionaries/po.json").then((module) => module.default),
 };
 
 export const getDictionary = async (locale: Locale) => dictionaries[locale]?.() ?? dictionaries.en();
