e8b05392651c55c3545ae928f85e7761bf768f13
Niihad <achyle03@gmail.com>
2025-11-11 17:34:02 +0100

    fix bug hearder in page function

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,17 @@
 import { redirect } from "next/navigation";
 import { i18n, Locale } from "@/i18n/i18n-config";
 import { match as matchLocale } from "@formatjs/intl-localematcher";
+import { headers } from "next/headers";
 
-export default function RootPage({ headers }: { headers?: Headers }) {
-  let locale: Locale = i18n.defaultLocale as Locale;
+export default function RootPage() {
+  const headersList = headers();
+  const acceptLang = headersList.get("accept-language") || "";
 
-  if (headers) {
-    const acceptLang = headers.get("accept-language") || "";
-    locale = matchLocale(
-      [acceptLang],
-      i18n.locales,
-      i18n.defaultLocale
-    ) as Locale;
-  }
+  const locale: Locale = matchLocale(
+    [acceptLang],
+    i18n.locales,
+    i18n.defaultLocale
+  ) as Locale;
 
-  redirect(`/${locale}`); // redirection côté serveur vers la langue détectée
+  redirect(`/${locale}`);
 }
