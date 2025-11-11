5b82c9ecbf6cf73862274b3f650f049cfb63eb57
Niihad <achyle03@gmail.com>
2025-11-11 18:07:52 +0100

    add metadata to layout racine

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,38 @@
+// /app/layout.tsx
 import { ReactNode } from "react";
 
 export const metadata = {
-  title: "Ora Dental",
-  description: "Cabinet dentaire ORA",
+  title: "Cabinet dentaire ORA : Dentiste et Orthodontiste à Differdange",
+  description: "Ora dental practice website",
+  icons: {
+    icon: "https://www.oradental.lu/assets/icon.png",
+  },
+  openGraph: {
+    title: "Cabinet dentaire ORA : Dentiste et Orthodontiste à Differdange",
+    type: "website",
+    siteName: "Cabinet dentaire ORA : Dentiste et Orthodontiste à Differdange",
+    images: "https://www.oradental.lu/assets/cabinet.png",
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="fr">
+      <head>
+        {/* Si tu veux ajouter JSON-LD Schema pour Google */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              name: "Ora Dental",
+              url: "https://www.oradental.lu",
+              logo: "https://www.oradental.lu/assets/icon.png",
+            }),
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
